25e10f6bbced589416384a9b86c37c0868b5b6c4
ayanhasnain03 <ayanhasnain2572006@gmail.com>
2025-11-22 13:28:35 +0530

    largest element in array

--- array/easy/largestElem.cpp
+++ array/easy/largestElem.cpp
@@ -1,3 +1,13 @@
-int main(){
-    cout<<"git setup"<<endl;
-}
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int maxi = arr[0];
+        for(int x :arr){
+            if(x > maxi){
+                maxi = x;
+            }
+        }
+         return maxi;
+    }
+
+};
